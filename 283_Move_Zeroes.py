4f471b06152ad293757bbdaa679102351d9b9d37
Crystal9202 <jpkoq18@gmail.com>
2021-09-25 00:18:12 +0800

    Add 283_Move_Zeroes.py

---
+++ 283_Move_Zeroes.py
@@ -0,0 +1,16 @@
+#時間複雜度 : O(n)
+class Solution:
+    def moveZeroes(self, nums):
+        start = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[start] =nums[i]
+                start += 1
+        for i in range(len(nums)):
+            if i >= start:
+                nums[i]= 0
+        return nums
+
+S=Solution()
+print(S.moveZeroes([0,1,0,3,12]))
+    
