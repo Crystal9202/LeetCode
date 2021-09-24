d05894776e45f9651078a935565302591d08845f
Crystal9202 <jpkoq18@gmail.com>
2021-09-24 23:56:22 +0800

    Add 027_Remove_Element.py

---
+++ 027_Remove_Element.py
@@ -0,0 +1,21 @@
+# 時間複雜度: 使用 python 內建列表方法，所以不看時間複雜度
+# class Solution:
+#     def removeElement(self, nums, val):
+#         while nums.count(val):
+#             nums.remove(val)
+#         return len(nums)
+      
+# S=Solution()
+# print(S.removeElement([0,1,2,2,3,0,4,2],2))
+
+#時間複雜度: O(n)
+class Solution:
+    def removeElement(self, nums, val):
+        start =0 
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[start] =nums[i]
+                start +=1
+        return start
+S =Solution()
+print(S.removeElement([3,2,2,3],3))
