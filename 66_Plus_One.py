8c4ca46a0e081e87d0fdab3447ee4177b828ecfa
Crystal9202 <jpkoq18@gmail.com>
2021-09-28 18:26:58 +0800

    Add 66_Plus_One.py

---
+++ 66_Plus_One.py
@@ -0,0 +1,12 @@
+class Solution:
+    def plusOne(self, digits):
+        for i in range(len(digits)-1,-1,-1):
+            digits[i] += 1 
+            if digits[i] != 10 :
+                return digits
+            digits[i] =  0
+        return [1] + digits
+
+S = Solution()
+print(S.plusOne([4,3,2,1]))
+        
