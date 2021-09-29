a175fedab2000a873d54586835a9cb1021d942fc
Crystal9202 <jpkoq18@gmail.com>
2021-09-29 17:35:21 +0800

    Add 989_Add_to_Array-Form_of_Integer.py

---
+++ 989_Add_to_Array-Form_of_Integer.py
@@ -0,0 +1,15 @@
+#時間複雜度 :O(max(n,log k))
+class Solution:
+    def addToArrayForm(self, num, k ) :
+        for i in range(len(num)-1,-1,-1):
+            num[i] += k 
+            if num[i] < 10 :
+                return num 
+            k , num[i] = divmod(num[i] , 10 )   # k , num[i]=num[i]//10,num[i]%10
+        while k >= 10 :
+            k , m =divmod(k , 10 )
+            num = [m] +num 
+        return [k] + num 
+
+S = Solution()
+print(S.addToArrayForm([1,2,0,0], k = 34))
