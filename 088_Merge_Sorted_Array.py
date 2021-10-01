91f6d5e770fa51a7066515ffc11e63212b46a067
Crystal9202 <jpkoq18@gmail.com>
2021-10-01 09:27:17 +0800

    Add 088_Merge_Sorted_Array.py

--- 088_Merge_Sorted_Array.py
+++ 088_Merge_Sorted_Array.py
@@ -1,3 +1,4 @@
+#時間複雜度: O(max(m,n))
 class Solution:
     def merge(self, nums1,m , nums2 ,n ):
         while m > 0 and n > 0 :
@@ -8,7 +9,3 @@ class Solution:
                 nums1[m+n-1] = nums2[n-1]
                 n -= 1
         nums1[:n] = nums2[:n]
-        return nums1
-
-S = Solution()
-print(S.merge(nums1=[1,2,3,0,0,0], m = 3,  nums2=[2,5,6] , n = 3))
