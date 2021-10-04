3f2e58b5e5a1161d8bca58000eca99cf5ac3cd41
Crystal9202 <jpkoq18@gmail.com>
2021-10-04 08:21:18 +0800

    Add 167_Two_Sum_II_Input_array_is_sorted.py

---
+++ 167_Two_Sum_II_Input_array_is_sorted.py
@@ -0,0 +1,14 @@
+# 時間複雜度 : O(n)
+# 空間複雜度 : O(n)
+class Solution:
+    def twoSum(self, numbers, target):
+        data = {}
+        for i in range(len(numbers)):
+            if target - numbers[i] not in data:
+                data[numbers[i]] = i+1 
+            else:
+                return [ data[target-numbers[i]] , i+1]
+
+S = Solution()
+print(S.twoSum([2,7,11,15],9))
+
