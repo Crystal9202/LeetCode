5795ae6e3042c0b9ac8d69f31f5525624a97ed1e
Crystal9202 <jpkoq18@gmail.com>
2021-10-05 09:21:39 +0800

    Add 219_Contains_Duplicate_II.py

---
+++ 219_Contains_Duplicate_II.py
@@ -0,0 +1,37 @@
+# 時間複雜度: O(n)
+# 空間複雜度: O(n)
+
+class Solution:
+    def containsNearbyDuplicate(self, nums, k):
+        data = {}
+        for i ,val in enumerate(nums):
+            if val not in data:
+                data[val] = i 
+            else:
+                if abs(data[val]-i) <= k :
+                    return True
+                else:
+                    data[val] = i 
+        return False
+S =Solution()
+print(S.containsNearbyDuplicate([1,2,3,1], k = 3))
+
+
+# 時間複雜度: O(n)
+# 空間複雜度: O(n)
+# 使用 enumerate() 可以寫得更簡化
+class Solution:
+    def containsNearbyDuplicate(self, nums, k):
+        data = {}
+        for i in range(len(nums)):
+            if nums[i] not in data:
+                data[nums[i]] = i
+            else:
+                if abs(data[nums[i]] - i) <= k:
+                    return True
+                else:
+                    data[nums[i]] = i
+        return False
+
+p = Solution()
+print(p.containsNearbyDuplicate([1,2,3,1,2,3], 2))
