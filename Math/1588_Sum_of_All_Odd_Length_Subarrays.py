e1191aaac88e2dce9b84865ee8be7331c081cb03
Crystal9202 <jpkoq18@gmail.com>
2021-10-21 15:34:20 +0800

    Add 1588_Sum_of_All_Odd_Length_Subarrays.py

---
+++ Math/1588_Sum_of_All_Odd_Length_Subarrays.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sumOddLengthSubarrays(self, arr):
+        res = 0
+        freq = 0
+        n = len(arr)
+        
+        for i in range(len(arr)):
+            freq = freq - (i+1)//2 + (n-i+1)//2
+            res += arr[i]*freq
+        return res
+S =Solution()
+print(S.sumOddLengthSubarrays([1,4,2,5,3]))
