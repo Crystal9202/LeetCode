6b68e3df96626e9da74578590dda663b8841ce62
Crystal9202 <jpkoq18@gmail.com>
2021-10-22 11:47:10 +0800

    Add 597_Friend_Requests_I_Overall_Acceptance_Rate.sql

---
+++ Database/597_Friend_Requests_I_Overall_Acceptance_Rate.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+
+select 
+round(
+    ifnull(
+        (select count(distinct requester_id , accepter_id)
+        from RequestAccepted)
+/
+(select count(distinct sender_id,send_to_id)
+    from FriendRequest)
+        ,0) ,2) as  accept_rate ;
