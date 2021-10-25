760876865de08a07850c1c6f869cdf876c1a86bd
Crystal9202 <jpkoq18@gmail.com>
2021-10-25 10:44:07 +0800

    Add 20_Valid_Parentheses.py

---
+++ Stack/20_Valid_Parentheses.py
@@ -0,0 +1,17 @@
+# 時間複雜度 :O(n)
+# 空間複雜度 :O(n)
+class Solution:
+    def isValid(self, s ):  
+        mapping = { ")" : "("  , "}" : "{" , "]" : "["  }
+        stack = []
+
+        for i in s :
+            if i in mapping.values():
+                stack.append(i)
+            elif i in mapping.keys():
+                if stack ==[] or mapping[i] != stack.pop() :
+                    return False
+        return stack ==[]
+        
+S = Solution()
+print(S.isValid("()"))
