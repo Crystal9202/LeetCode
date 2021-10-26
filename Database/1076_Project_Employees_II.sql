3781dd893b76078205a0085802c903a1faa95df6
Crystal9202 <jpkoq18@gmail.com>
2021-10-26 12:12:39 +0800

    Add 1076_Project_Employees_II.sql

---
+++ Database/1076_Project_Employees_II.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+
+select  project_id 
+from Project
+group by project_id 
+having count(employee_id ) = 
+(
+select count(employee_id)
+from Project
+group by project_id
+order by count(employee_id) desc
+limit 1 
+)  ; 
