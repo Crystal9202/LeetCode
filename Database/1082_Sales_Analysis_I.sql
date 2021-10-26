76b13fd15f5e926f5b814220489d37505308d446
Crystal9202 <jpkoq18@gmail.com>
2021-10-26 13:28:39 +0800

    Add 1082_Sales_Analysis_I.sql

---
+++ Database/1082_Sales_Analysis_I.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+
+select seller_id 
+from Sales 
+group by seller_id 
+having sum(price) = 
+(
+select sum(price) 
+from Sales
+group by seller_id 
+order by sum(price) desc
+limit 1 )  ;
