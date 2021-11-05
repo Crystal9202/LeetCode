6062d0f1cc65b9dc3d3ddfcc5520682303d23e5a
Crystal9202 <jpkoq18@gmail.com>
2021-11-05 20:47:21 +0800

    Add 1694_Reformat_Phone_Number.py

---
+++ String/1694_Reformat_Phone_Number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def reformatNumber(self, number: str) -> str:
+        number = number.replace("-" ,'').replace(" ","")
+        res = []
+        
+        for i in range(0,len(number),3) :
+            if len(number) -i != 4 :
+                res.append(number[i:i+3])
+            else:
+                res.extend([number[i:i+2],number[i+2:]])
+                break
+                
+        return "-".join(res)
+S = Solution()
+print(S.reformatNumber("1-23-45 6")) 
