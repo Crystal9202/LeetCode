8c4c2cc24c6d698669098a804397304089422b1f
Crystal9202 <jpkoq18@gmail.com>
2021-11-06 16:43:31 +0800

    Add 202_Happy_Number.py

---
+++ 202_Happy_Number.py
@@ -0,0 +1,12 @@
+# 時間複雜度: O(log(n))
+# 空間複雜度: O(log(n))
+class Solution:
+    def isHappy(self, n):
+        seen = set()       
+        while n not in seen :
+            seen.add(n)
+            n = sum(int(i)**2 for i in str(n))
+            
+        return n == 1 
+S = Solution()
+print(S.isHappy(19))
