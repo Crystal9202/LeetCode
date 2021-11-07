588a89a595e4900a1e00f308528c65b86f995b52
Crystal9202 <jpkoq18@gmail.com>
2021-11-07 19:41:39 +0800

    Add 1623_All_Valid_Triplets_That_Can_Represent_a_Country.sql

---
+++ Database/1623_All_Valid_Triplets_That_Can_Represent_a_Country.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+
+select a.student_name as member_A  , b.student_name as member_B  ,c.student_name as member_C
+from SchoolA as a  join SchoolB as b  on a.student_name != b.student_name  and a.student_id != b.student_id
+                    join SchoolC as c  on a.student_name !=   c.student_name  and a.student_id != c.student_id   
+                                                            and b.student_name != c.student_name and b.student_id != c.student_id ; 
