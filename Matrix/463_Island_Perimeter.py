14a20d770e3424906077da1ca6119ca1409247a8
Crystal9202 <jpkoq18@gmail.com>
2021-11-13 21:39:26 +0800

    Add 463_Island_Perimeter.py

--- Matrix/463_Island_Perimeter.py
+++ Matrix/463_Island_Perimeter.py
@@ -1,7 +1,7 @@
 # 時間複雜度: O(m*n)
 # 空間複雜度: O(1)
 class Solution:
-    def islandPerimeter(self, grid: List[List[int]]) -> int:
+    def islandPerimeter(self, grid) -> int:
         r , c = len(grid) ,len(grid[0])
         result = 0
         
@@ -16,3 +16,5 @@ class Solution:
                     if col -1 >=0 and grid[row][col-1] == 1 :
                         result -= 2
         return result
+S =Solution()
+print(S.islandPerimeter([[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]))
