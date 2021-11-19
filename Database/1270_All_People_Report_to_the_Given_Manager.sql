10d2c0e64fabfbe7ea39130a3d535855fe4ff440
Crystal9202 <jpkoq18@gmail.com>
2021-11-19 18:58:19 +0800

    Add 1270_All_People_Report_to_the_Given_Manager.sql

---
+++ Database/1270_All_People_Report_to_the_Given_Manager.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+
+select e1.employee_id
+from Employees as e1  join Employees as e2  on e1.manager_id = e2.employee_id 
+                      join Employees as e3  on e2.manager_id = e3.employee_id
+                    where e1.employee_id <> 1  and (e1.manager_id =1 or e2.manager_id = 1 or e3.manager_id = 1)  ;
