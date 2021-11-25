c6d398c03fddf2ca2896638bd4ea192e74c04ca8
Crystal9202 <jpkoq18@gmail.com>
2021-11-25 21:54:56 +0800

    Add 1445_Apples_Oranges.sql

---
+++ Database/1445_Apples_Oranges.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+
+select sale_date , 
+        sum(case when fruit ="apples" then sold_num
+            else -sold_num
+            end) as diff
+from Sales
+group by sale_date ;
